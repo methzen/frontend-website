f07d5021bfe252c17714b4b6ac68d40e8f7235eb
methzen <diamohaminsa@gmail.com>
2021-12-16 15:34:05 +0100

    Google verification ID

--- next.config.js
+++ next.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     env : {
         "DEV_API_URL" : "http://localhost:9000",
-        "PRODUCTION_API_URL": "https://www.api.mouhamadoudia.fr"
+        "PRODUCTION_API_URL": "https://www.api.mouhamadoudia.fr",
         "GOOGLE_VERIFICATION_ID": "-i0ge6GT1_bmt498IqJC9wkY89wKXcyRbykxPVwnCP8"
     }
 }
