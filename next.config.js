ba3c7297c1fa04eb76c396043f5d77acb8cfa455
methzen <diamohaminsa@gmail.com>
2021-12-16 16:04:38 +0100

    Add Bing site verification meta tag to the headMetadata.js component.

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ module.exports = {
     env : {
         "DEV_API_URL" : "http://localhost:9000",
         "PRODUCTION_API_URL": "https://www.api.mouhamadoudia.fr",
-        "GOOGLE_VERIFICATION_ID": "-i0ge6GT1_bmt498IqJC9wkY89wKXcyRbykxPVwnCP8"
+        "GOOGLE_VERIFICATION_ID": "-i0ge6GT1_bmt498IqJC9wkY89wKXcyRbykxPVwnCP8",
+        "BING_VERIFICATION_ID": "320EE17964C6F08B23981F78063CB919"
     }
 }
