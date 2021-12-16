5d8b1402235bb0842a027a249792b4e27b145bfb
methzen <diamohaminsa@gmail.com>
2021-12-16 15:28:45 +0100

    Add Google site verification meta tag to the headMetadata.js component.

--- next.config.js
+++ next.config.js
@@ -2,5 +2,6 @@ module.exports = {
     env : {
         "DEV_API_URL" : "http://localhost:9000",
         "PRODUCTION_API_URL": "https://www.api.mouhamadoudia.fr"
+        "GOOGLE_VERIFICATION_ID": "-i0ge6GT1_bmt498IqJC9wkY89wKXcyRbykxPVwnCP8"
     }
 }
